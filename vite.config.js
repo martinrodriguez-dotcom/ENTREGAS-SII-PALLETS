93b18bfe6a04e31c80c9034e8c66aaedf28f5471
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2025-12-22 14:33:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Asegura que la salida sea en la carpeta 'dist' para Netlify
     outDir: 'dist',
+    // Optimiza la generación de assets
+    assetsDir: 'assets',
   },
+  // Define que la carpeta public contiene los archivos estáticos de la PWA
   publicDir: 'public',
 })
