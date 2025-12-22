17ab57ee72ed8907e8aeeeb0eb6aa2b1ecbaf766
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2025-12-22 14:35:12 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Importante para Tailwind
+import './index.css' // Esto carga Tailwind CSS para toda la app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
